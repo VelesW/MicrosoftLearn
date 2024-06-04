4940c98eaa422796e11f453fdbb9b86862326089
VelesW <michalwilk296@gmail.com>
2024-06-04 09:58:54 +0200

    Apply string interpolation to a form letter

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,10 +1,31 @@
-﻿string paymentId = "769C";
- string payeeName = "Mr. Stephen Ortega";
- string paymentAmount = "$5,000.00";
+﻿string customerName = "Ms. Barros";
 
- var formattedLine = paymentId.PadRight(6);
- formattedLine += payeeName.PadRight(24);
- formattedLine += paymentAmount.PadLeft(10);
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
- Console.WriteLine("1234567890123456789012345678901234567890");
- Console.WriteLine(formattedLine);
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format($"{currentReturn:P}").PadRight(10);
+comparisonMessage += String.Format($"{currentProfit:C}").PadRight(20);
+
+comparisonMessage += "\n";
+
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format($"{newReturn:P}").PadRight(10);
+comparisonMessage += String.Format($"{newProfit:C}").PadRight(20);
+
+Console.WriteLine(comparisonMessage);
