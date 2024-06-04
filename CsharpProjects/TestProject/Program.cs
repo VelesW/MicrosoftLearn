d2e84254365c44876cf7a1f093fdb48a8b1049ce
VelesW <michalwilk296@gmail.com>
2024-06-04 09:28:51 +0200

    Discover padding and alignment

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,11 +1,10 @@
-﻿int invoiceNumber = 1201;
-decimal productShares = 25.4568m;
-decimal subtotal = 2750.00m;
-decimal taxPercentage = .15825m;
-decimal total = 3185.19m;
+﻿string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
 
-Console.WriteLine($"Invoice Number: {invoiceNumber}");
-Console.WriteLine($"\tShares: {productShares:N3} Product");
-Console.WriteLine($"\t\tSub Total: {subtotal:C}");
-Console.WriteLine($"\t\t\tTax: {taxPercentage:P2}");
-Console.WriteLine($"\t\tTotal Billed: {total:C}");
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
+
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine);
