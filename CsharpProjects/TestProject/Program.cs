5aeec2b4b51db822502b570a5b7f4ffb47fb2a4c
VelesW <michalwilk296@gmail.com>
2024-06-05 08:37:45 +0200

    Extract, replace, and remove data from an input string

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,8 +1,31 @@
-﻿// string data = "12345John Smith          5000  3  ";
-// string removed = data.Remove(5, 20);
-// Console.WriteLine(removed);
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+const string openingSpan = "<span>";
+const string closingSpan = "</span>";
+
+
+string quantity = "";
+string output = "";
+
+// Get the quantity from input
+int startingPosition = input.IndexOf(openingSpan) + openingSpan.Length;
+int closingPosition = input.IndexOf(closingSpan);
+int length = closingPosition - startingPosition;
+quantity = input.Substring(startingPosition, length);
+
+// Remove divs from input
+const string openingDiv = "<div>";
+const string closingDiv = "</div>";
+
+output = input.Replace(openingDiv, "");
+output = output.Replace(closingDiv, "");
+
+// Replace trade with reg
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+
+output = output.Replace(tradeSymbol, regSymbol);
+
+
+Console.WriteLine($"Qunatity: {quantity}");
+Console.WriteLine($"Output: {output}");
