c88890076bccb86d51910a2345f7cb2ac7ff5588
VelesW <michalwilk296@gmail.com>
2024-06-08 09:21:21 +0200

    Create your first method

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,31 +1,11 @@
-﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+﻿void DisplayRandomNumbers()
+{
+    Random random = new Random();
 
-const string openingSpan = "<span>";
-const string closingSpan = "</span>";
+    for (int i = 0; i < 5; i++)
+        Console.WriteLine(random.Next(1,100));
+    Console.WriteLine();
+}
 
-
-string quantity = "";
-string output = "";
-
-// Get the quantity from input
-int startingPosition = input.IndexOf(openingSpan) + openingSpan.Length;
-int closingPosition = input.IndexOf(closingSpan);
-int length = closingPosition - startingPosition;
-quantity = input.Substring(startingPosition, length);
-
-// Remove divs from input
-const string openingDiv = "<div>";
-const string closingDiv = "</div>";
-
-output = input.Replace(openingDiv, "");
-output = output.Replace(closingDiv, "");
-
-// Replace trade with reg
-const string tradeSymbol = "&trade;";
-const string regSymbol = "&reg;";
-
-output = output.Replace(tradeSymbol, regSymbol);
-
-
-Console.WriteLine($"Qunatity: {quantity}");
-Console.WriteLine($"Output: {output}");
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
