ea7ca024de3a1da044394a0afece5d4c44a5d5d6
VelesW <michalwilk296@gmail.com>
2024-06-10 09:17:34 +0200

    Return arrays from methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,21 +1,43 @@
-﻿string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
+﻿int target = 30;
+int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+int[,] result = TwoCoins(coins, target);
 
-Console.WriteLine("Is it a palindrome?");
-foreach (string word in words) 
+if (result.Length == 0) 
 {
-    Console.WriteLine($"{word}: {IsPalindrome(word)}");
+    Console.WriteLine("No two coins make change");
+} 
+else 
+{
+    Console.WriteLine("Change found at positions:");
+    for (int i = 0; i < result.GetLength(0); i++) 
+    {
+        if (result[i,0] == -1) 
+        {
+            break;
+        }
+        Console.WriteLine($"{result[i,0]},{result[i,1]}");
+    }
 }
 
-bool IsPalindrome(string word) 
+int[,] TwoCoins(int[] coins, int target)
 {
-    int start = 0;
-    int end = word.Length - 1;
-    while (start < end)
+    int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+    int count = 0;
+    for (int i = 0; i < coins.Length; i++)
     {
-        if (word[start] != word[end])
-            return false;
-        start++;
-        end--;
+        for (int j = i + 1; j < coins.Length; j++)
+        {
+            if (coins[i] + coins[j] == target)
+            {
+                result[count, 0] = i;
+                result[count, 1] = j;
+                count++;
+            }
+            if (count == result.GetLength(0))
+            {
+                return result;
+            }
+        }
     }
-    return true;
+    return (count == 0) ? new int[0,0] : result;
 }
