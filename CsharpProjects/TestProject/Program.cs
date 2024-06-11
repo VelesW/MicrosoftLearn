3fe3dda5d1524c7e7943a740425da2ea7e08a73c
VelesW <michalwilk296@gmail.com>
2024-06-11 18:53:47 +0200

    Create a method to display the results

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -7,11 +7,9 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-RandomizeAnimals();
-string[,] group = AssignGroup();
-Console.WriteLine("School A");
-// PrintGroup(group);
-
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
 
 void RandomizeAnimals()
 {
@@ -29,16 +27,38 @@ void RandomizeAnimals()
 
 string[,] AssignGroup(int groups = 6)
 {
-    string[,] result = new string[groups, pettingZoo.Length/groups];
+    string[,] result = new string[groups, pettingZoo.Length / groups];
 
     int start = 0;
     for (int i = 0; i < groups; i++)
     {
         for (int j = 0; j < result.GetLength(1); j++)
         {
-            result[i,j] = pettingZoo[start++];
+            result[i, j] = pettingZoo[start++];
         }
     }
 
     return result;
+}
+
+void PrintGroup(string[,] group)
+{
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+        {
+            Console.Write($"{group[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+    Console.WriteLine();
 }
