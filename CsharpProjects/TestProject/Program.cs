03084d83feb72fccd3c8cd40e5a15dae4f29b87a
VelesW <michalwilk296@gmail.com>
2024-06-11 18:04:51 +0200

    Plan your code using methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,64 +1,13 @@
-﻿using System.Xml.XPath;
-using Microsoft.VisualBasic;
+﻿using System;
 
-Random random = new Random();
-
-Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay())
+string[] pettingZoo = 
 {
-    PlayGame();
-}
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
 
-void PlayGame()
-{
-    var play = true;
-
-    while (play)
-    {
-        var target = GetTarget();
-        var roll = RollDice();
-
-        Console.WriteLine($"Roll a number greater than {target} to win!");
-        Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose(roll, target));
-        Console.WriteLine("\nPlay again? (Y/N)");
-
-        play = ShouldPlay();
-    }
-}
-
-bool ShouldPlay()
-{
-    string yesOrNo = "";
-    while (yesOrNo == "")
-    {
-        string? result = Console.ReadLine();
-        if (result != null)
-        {
-            yesOrNo = result.ToLower();
-            if (yesOrNo == "y" || yesOrNo == "yes")
-                return true;
-            else if (yesOrNo == "n" || yesOrNo == "no")
-                return false;
-            else
-                yesOrNo = "";
-        }
-    }
-    return false;
-}
-
-string WinOrLose(int roll, int target)
-{
-    return (roll > target) ? "You win!" : "You lose!";
-}
-
-
-int GetTarget() 
-{
-    return random.Next(1, 6);
-}
-
-int RollDice() 
-{
-    return random.Next(1, 7);
-}
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
