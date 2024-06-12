dc1b85650f3cae94cabc0e68b6fd694a6881a326
VelesW <michalwilk296@gmail.com>
2024-06-12 15:02:45 +0200

    Add code to modify movement

--- Challenge-project-Create-methods-in-CSharp-main/Starter/Program.cs
+++ Challenge-project-Create-methods-in-CSharp-main/Starter/Program.cs
@@ -15,8 +15,8 @@ int foodX = 0;
 int foodY = 0;
 
 // Available player and food strings
-string[] states = {"('-')", "(^-^)", "(X_X)"};
-string[] foods = {"@@@@@", "$$$$$", "#####"};
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
 
 // Current player string displayed in the Console
 string player = states[0];
@@ -25,20 +25,38 @@ string player = states[0];
 int food = 0;
 
 InitializeGame();
-while (!shouldExit) 
+while (!shouldExit)
 {
-    Move(nonDirectional: true);
-    TerminateOnResize();
+    if (TerminalResized())
+    {
+        Console.Clear();
+        Console.WriteLine("Console was resized. Program exiting.");
+        shouldExit = true;
+    }
+    else
+    {
+        if (PlayerIsFaster())
+            Move(speed: 3, nonDirectional: true);
+        else if (PlayerIsSick())
+            FreezePlayer();
+        else
+            Move(nonDirectional: true);
+        if (IsFoodConsumed())
+        {
+            ChangePlayer();
+            ShowFood();
+        }
+    }
 }
 
 // Returns true if the Terminal was resized 
-bool TerminalResized() 
+bool TerminalResized()
 {
     return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
 // Displays random food at a random location
-void ShowFood() 
+void ShowFood()
 {
     // Update food to a random index
     food = random.Next(0, foods.Length);
@@ -53,7 +71,7 @@ void ShowFood()
 }
 
 // Changes the player to match the food consumed
-void ChangePlayer() 
+void ChangePlayer()
 {
     player = states[food];
     Console.SetCursorPosition(playerX, playerY);
@@ -61,44 +79,44 @@ void ChangePlayer()
 }
 
 // Temporarily stops the player from moving
-void FreezePlayer() 
+void FreezePlayer()
 {
     System.Threading.Thread.Sleep(1000);
     player = states[0];
 }
 
 // Reads directional input from the Console and moves the player
-void Move(bool nonDirectional=false) 
+void Move(int speed = 1, bool nonDirectional = false)
 {
     int lastX = playerX;
     int lastY = playerY;
-    
-    switch (Console.ReadKey(true).Key) 
+
+    switch (Console.ReadKey(true).Key)
     {
         case ConsoleKey.UpArrow:
-            playerY--; 
+            playerY--;
             break;
-		case ConsoleKey.DownArrow: 
-            playerY++; 
+        case ConsoleKey.DownArrow:
+            playerY++;
             break;
-		case ConsoleKey.LeftArrow:  
-            playerX--; 
+        case ConsoleKey.LeftArrow:
+            playerX -= speed;
             break;
-		case ConsoleKey.RightArrow: 
-            playerX++; 
+        case ConsoleKey.RightArrow:
+            playerX += speed;
             break;
-		case ConsoleKey.Escape:     
-            shouldExit = true; 
+        case ConsoleKey.Escape:
+            shouldExit = true;
             break;
         default:
-            if(nonDirectional)
+            if (nonDirectional)
                 System.Environment.Exit(-1);
             break;
     }
 
     // Clear the characters at the previous position
     Console.SetCursorPosition(lastX, lastY);
-    for (int i = 0; i < player.Length; i++) 
+    for (int i = 0; i < player.Length; i++)
     {
         Console.Write(" ");
     }
@@ -110,16 +128,10 @@ void Move(bool nonDirectional=false)
     // Draw the player at the new location
     Console.SetCursorPosition(playerX, playerY);
     Console.Write(player);
-
-    if (IsFoodConsumed())
-    {
-        ShowFood();
-        player = states[food];
-    }
 }
 
 // Clears the console, displays the food and player
-void InitializeGame() 
+void InitializeGame()
 {
     Console.Clear();
     ShowFood();
@@ -127,19 +139,19 @@ void InitializeGame()
     Console.Write(player);
 }
 
-void TerminateOnResize()
-{
-    if (TerminalResized())
-    {
-        Console.Clear();
-        Console.WriteLine("Console was resized. Program exiting.");
-        shouldExit = true;
-    }  
-}
-
 bool IsFoodConsumed()
 {
+    if (PlayerIsFaster())
+        return (playerX <= foodX + 3 && playerX >= foodX) && playerY == foodY;
     return playerX == foodX && playerY == foodY;
 }
 
+bool PlayerIsFaster()
+{
+    return player.Equals(states[1]);
+}
 
+bool PlayerIsSick()
+{
+    return player.Equals(states[2]);
+}
